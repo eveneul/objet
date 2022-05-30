69529124f67a65637cc0f0449ed39bac93177647
오하늘 <nebulass@naver.com>
2022-05-31 00:49:09 +0900

    mainGallery 추가

---
+++ js/mainGallery.js
@@ -0,0 +1,42 @@
+const gallery = document.querySelector(".mainGallery .inner");
+
+const key = "c242a84b957d53602081a6a1c4adef5d";
+const base = "https://www.flickr.com/services/rest/?";
+const per_page = 50;
+const methodMyPhoto = "flickr.people.getPhotos";
+const myKey = "195703740@N02";
+const myPhotoUrl = `${base}method=${methodMyPhoto}&api_key=${key}&per_page=${per_page}&format=json&nojsoncallback=1&privacy_filter=1&user_id=${myKey}`;
+
+callImgDate(myPhotoUrl);
+
+function callImgDate(url) {
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((json) => {
+      let items = json.photos.photo;
+      createGallery(items);
+    });
+}
+
+function createGallery(items) {
+  let result = "";
+  items.forEach((item) => {
+    let imgSrcBig = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`;
+
+    result += `
+          <article class="images">
+            <img
+              src=${imgSrcBig}
+              alt="main gallery image"
+            />
+          </article>
+      `;
+  });
+
+  gallery.innerHTML = result;
+}
+
+const test = gallery.querySelector(".images");
+console.log(test);
